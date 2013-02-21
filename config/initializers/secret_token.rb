77580042f0a100ca137c418c68fcc5d79180624e
Terence Chow <terence.chow.1986@gmail.com>
2013-02-20 21:58:00 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikipediaGame::Application.config.secret_token = '66b86c0fb9c2cbc134c87f137e3c40df3fa288f974e3cd2492c76f802bb3b0edd7757c8442751822bf6d5f62591dd3e114e56913011e8a07acdb141be7650c1c'
